d310dc5ffb1f25ae747e6597e4f3782715ba906c
yelbuke <yelbuke@gmail.com>
2015-08-24 01:11:28 +0300

    converted models' __unicode__ functions to __str__ functions as all strings are natively considered Unicode on python3 . edited models' __str__ function's return values to give tip about foreignkeys.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import *
+
+admin.site.register( Company )
+admin.site.register( Country )
+admin.site.register( City )
+admin.site.register( Building )
+admin.site.register( DataCenterRoom )
+admin.site.register( Row )
+admin.site.register( Cabinet )
+admin.site.register( DeviceType )
+admin.site.register( Device )
--- main/models.py
+++ main/models.py
@@ -6,8 +6,8 @@ class Company( models.Model ):
 
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.iso_code))
+	def __str__( self ):
+		return "%s" % ( self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Companies"
@@ -17,58 +17,63 @@ class Country( models.Model ):
 	name		= models.CharField( max_length = 50 )
 	iso_code	= models.CharField( max_length = 2 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.iso_code))
+	def __str__( self ):
+		return "%s" % ( self.iso_code )
 	
 	class Meta:
 		verbose_name_plural = u"Countries"
 
 class City( models.Model ):
 
+	country		= models.ForeignKey( Country )
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s - %s" % ( self.country.iso_code , self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Cities"
 
 class Building( models.Model ):
 
+	city		= models.ForeignKey( City )
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s - %s" % ( self.city , self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Buildings"
 
 class DataCenterRoom( models.Model ):
 
+	building	= models.ForeignKey( Building )
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s - %s" % ( self.building , self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Data center rooms"
 
 class Row( models.Model ):
 
+	datacenterroom		= models.ForeignKey( DataCenterRoom )
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s - %s" % ( self.datacenterroom , self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Rows"
 
 class Cabinet( models.Model ):
 
+	row			= models.ForeignKey( Row )
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s - %s" % ( self.row , self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Cabinets"
@@ -77,8 +82,8 @@ class DeviceType( models.Model ):
 
 	name		= models.CharField( max_length = 50 )
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s" % ( self.name )
 	
 	class Meta:
 		verbose_name_plural = u"Device Types"
@@ -92,10 +97,11 @@ class Device( models.Model ):
 	owner		= models.ForeignKey( Company , related_name='owner' )
 	manager		= models.ForeignKey( Company , related_name='manager' )
 	cabinet		= models.ForeignKey( Cabinet )
-	rack		= models.SmallIntegerField()
+	rack_first	= models.SmallIntegerField(default=1)
+	rack_last	= models.SmallIntegerField(null=True,blank=True)
 
-	def __unicode__( self ):
-		return u"%s" % ( smart_unicode(self.name))
+	def __str__( self ):
+		return "%s %s %s" % ( self.type , self.brand , self.model )
 	
 	class Meta:
 		verbose_name_plural = u"Devices"
